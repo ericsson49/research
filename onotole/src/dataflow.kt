57ea91e38df364ad4b466b31d8e1ed09e08bb02b
ericsson <avv49@mail.ru>
2020-11-06 15:26:07 +0300

    Some fixes and improvements

--- onotole/src/dataflow.kt
+++ onotole/src/dataflow.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 typealias FlowFun = (VarSet) -> VarSet
 
 sealed class FFunc<T>
@@ -37,6 +39,14 @@ fun <N,T> solve(p: DFAProblem<N,T>): Map<N,T> {
   return results.toMap()
 }
 
+fun <K,V> reverse(deps: Map<K,Collection<V>>): Map<V,List<K>> {
+  val res = mutableMapOf<V,MutableSet<K>>()
+  deps.forEach { (n, succ) ->
+    succ.forEach { m -> res.getOrPut(m, ::mutableSetOf).add(n) }
+  }
+  return res.mapValues { it.value.toList() }
+}
+
 class CFG<T> {
   private var completed = false
   val transitions = mutableMapOf<T,List<T>>()
@@ -68,8 +78,19 @@ class CFG<T> {
       }
       return _reverse!!
     }
+  private var _first: T? = null
+  val first: T
+    get() {
+      if (!completed)
+        fail("should be completed first")
+      if (_first == null) {
+        _first = transitions.keys.minus(reverse.keys).toList()[0]
+      }
+      return _first!!
+    }
   fun addInstr(i: T, next: T) = addInstr(i, listOf(next))
   fun addInstr(i: T, next: List<T>): T {
+    if (completed) fail("graph is in the completed state")
     transitions[i] = next
     return i
   }
@@ -81,79 +102,144 @@ fun <T, U, A> List<T>.foldRightWithAcc(init: U, acc: A, f: (T, U, A) -> U): U {
 }
 
 interface NodeProcessor<T> {
-  fun enterInstr(cfg: CFG<T>): T
-  fun assignInstr(cfg: CFG<T>, targets: Set<identifier>, refs: Set<String>): T
-  fun branchInstr(cfg: CFG<T>, refs: Set<String>): T
-  fun exitInstr(cfg: CFG<T>): T
+  fun mkInstr(targets: Set<identifier> = emptySet(), refs: Set<String> = emptySet()): T
 }
+
+sealed class EdgeSource
+data class BlockIndex(val block: List<Stmt>, val index: Int): EdgeSource() {
+  override fun equals(other: Any?): Boolean {
+    return this === other || other is BlockIndex && block === other.block && index == other.index
+  }
+}
+/*data class LoopTest(val loop: Stmt, val exit: Boolean): EdgeSource() {
+  override fun equals(other: Any?): Boolean {
+    return this === other || other is LoopTest && loop === other.loop && exit == other.exit
+  }
+}*/
+class Undef: EdgeSource()
 
 class CFGBuilder<T>(val proc: NodeProcessor<T>) {
   val cfg = CFG<T>()
   val tmpVars = mutableSetOf<String>()
+  val stmtToInstrMap = IdentityHashMap<Stmt,Pair<T,T>>()
+  val edgeLabeling = mutableMapOf<Pair<T,T>,EdgeSource>()
+  val loopTargets = mutableListOf<Pair<T,T>>()
   companion object {
-    fun <T> makeCFG(f: FunctionDef, proc: NodeProcessor<T>): CFG<T> {
+    fun <T> makeCFG(f: FunctionDef, proc: NodeProcessor<T>): Triple<CFG<T>,Map<Stmt,Pair<T,T>>,Map<Pair<T,T>,EdgeSource>> {
       val builder = CFGBuilder(proc)
       val cfg = builder.cfg
-      val exit = builder.cfg.addInstr(proc.exitInstr(builder.cfg), emptyList())
+      val exit = cfg.addInstr(proc.mkInstr(), emptyList())
       val first = builder.processStmts(f.body, exit)
-      val pre = cfg.addInstr(proc.assignInstr(cfg, f.args.args.map { it.arg }.toSet(), emptySet()), first)
-      cfg.addInstr(proc.enterInstr(cfg), pre)
+      val pre = builder.addInstr(proc.mkInstr(f.args.args.map { it.arg }.toSet(), emptySet()), first, BlockIndex(f.body, 0))
+      cfg.addInstr(proc.mkInstr(), pre)
       cfg.complete()
-      return cfg
+      return Triple(cfg, builder.stmtToInstrMap, builder.edgeLabeling)
     }
   }
 
-  fun processStmt(s: Stmt, next: T): T {
-    return when(s) {
+  fun addInstr(i: T, next: T, l: EdgeSource): T {
+    val res = cfg.addInstr(i, next)
+    edgeLabeling[i to next] = l
+    return res
+  }
+
+  fun addInstr(i: T, next: List<T>, labels: List<EdgeSource>): T {
+    val res = cfg.addInstr(i, next)
+    next.zip(labels).forEach { (n, l) ->
+      edgeLabeling[i to n] = l
+    }
+    return res
+  }
+
+  fun pushLoop(head: T, exit: T) {
+    loopTargets.add(Pair(head, exit))
+  }
+  fun popLoop() {
+    loopTargets.removeAt(loopTargets.size-1)
+  }
+
+  fun processStmt(block: List<Stmt>, i: Int, next: T): T {
+    val s = block[i]
+    val index = BlockIndex(block,i+1)
+    val res = when(s) {
+      is Expr -> {
+        addInstr(proc.mkInstr(emptySet(), liveVarAnalysis(s.value)), next, index)
+      }
+      is Assert -> {
+        val refs = liveVarAnalysis(s.test).plus(s.msg?.let(::liveVarAnalysis) ?: emptySet())
+        addInstr(proc.mkInstr(emptySet(), refs), next, index)
+      }
       is Assign -> {
         if (s.targets.size != 1)
           fail("unsupported")
         val target = s.targets[0]
         val vars = getVarNamesInStoreCtx(target).toSet()
         val refs = liveVarAnalysis(s.value)
-        cfg.addInstr(proc.assignInstr(cfg, vars, refs), next)
+        addInstr(proc.mkInstr(vars, refs), next, index)
       }
       is AnnAssign -> {
         val target = s.target
         val vars = getVarNamesInStoreCtx(target).toSet()
         val refs = liveVarAnalysis(s.value!!)
-        cfg.addInstr(proc.assignInstr(cfg, vars, refs), next)
+        addInstr(proc.mkInstr(vars, refs), next, index)
       }
       is AugAssign -> {
         val target = s.target
         val vars = getVarNamesInStoreCtx(target).toSet()
         val refs = liveVarAnalysis(s.value).plus(vars)
-        cfg.addInstr(proc.assignInstr(cfg, vars, refs), next)
+        addInstr(proc.mkInstr(vars, refs), next, index)
       }
       is If -> {
         val elseBegin = processStmts(s.orelse, next)
         val bodyBegin = processStmts(s.body, next)
-        cfg.addInstr(proc.branchInstr(cfg, liveVarAnalysis(s.test)), listOf(bodyBegin, elseBegin))
+        addInstr(
+            proc.mkInstr(targets = emptySet(), refs = liveVarAnalysis(s.test)),
+            listOf(bodyBegin, elseBegin), listOf(BlockIndex(s.body, 0), BlockIndex(s.orelse, 0)))
       }
       is While -> {
-        val whileHead = proc.branchInstr(cfg, liveVarAnalysis(s.test))
+        val whileHead = proc.mkInstr(targets = emptySet(), refs = liveVarAnalysis(s.test))
+        pushLoop(whileHead, next)
         val bodyBegin = processStmts(s.body, whileHead)
-        cfg.addInstr(whileHead, listOf(bodyBegin, next))
+        popLoop()
+        addInstr(whileHead, listOf(bodyBegin, next), listOf(BlockIndex(s.body, 0), BlockIndex(block, i+1)))
       }
       is For -> {
         val newTmpVar = "tmp_" + tmpVars.size
         tmpVars.add(newTmpVar)
-        val preHead = proc.assignInstr(cfg, setOf(newTmpVar), liveVarAnalysis(s.iter))
-        val forHead = proc.branchInstr(cfg, setOf(newTmpVar))
+        val preHead = proc.mkInstr(setOf(newTmpVar), liveVarAnalysis(s.iter))
+        val forHead = proc.mkInstr(targets = emptySet(), refs = setOf(newTmpVar))
+        pushLoop(forHead, next)
         val bodyBegin = processStmts(s.body, forHead)
-        if (s.target !is Name)
+        popLoop()
+        if (!(s.target is Name || s.target is Tuple && s.target.elts.all { it is Name }))
           fail("not implemented yet")
-        val preBody = cfg.addInstr(proc.assignInstr(cfg, setOf(s.target.id), setOf(newTmpVar)), bodyBegin)
-        cfg.addInstr(forHead, listOf(preBody, next))
-        cfg.addInstr(preHead, forHead)
+        val names = getVarNamesInStoreCtx(s.target)
+        val preBody = addInstr(proc.mkInstr(names.toSet(), setOf(newTmpVar)), bodyBegin, /*Undef()*/BlockIndex(s.body, 0))
+        addInstr(forHead, listOf(preBody, next), listOf(/*BlockIndex(s.body, 0)*/Undef(), BlockIndex(block, i+1)))
+        addInstr(preHead, forHead, BlockIndex(block, i))
       }
       is Return -> {
-        s.value?.let { cfg.addInstr(proc.assignInstr(cfg, setOf("<return>"), liveVarAnalysis(it)), next) } ?: next
+        s.value?.let { addInstr(proc.mkInstr(setOf("<return>"), liveVarAnalysis(it)), next, BlockIndex(block, i)) } ?: next
+      }
+      is Continue -> {
+        loopTargets[loopTargets.size-1].first
+      }
+      is Break -> {
+        loopTargets[loopTargets.size-1].second
       }
       else -> fail("not implemented $s")
     }
+    stmtToInstrMap[s] = Pair(res, next)
+    return res
   }
-  fun processStmts(c: List<Stmt>, next: T) = c.foldRight(next, ::processStmt)
+
+  fun processStmts(block: List<Stmt>, next: T): T {
+    var currNext = next
+    block.indices.reversed().forEach { i ->
+      currNext = processStmt(block, i, currNext)
+    }
+    return currNext
+  }
 }
 
 fun <T> makeGenKill(gen: Set<T>, kill: Set<T>): RealFFunc<Set<T>> {
--- onotole/src/dataflow_analyses.kt
+++ onotole/src/dataflow_analyses.kt
@@ -1,13 +1,19 @@
 import java.nio.file.Files
 import java.nio.file.Paths
 
-sealed class Instr(val cfg: CFG<Instr>) {
-  override fun toString() = cfg.labeling[this]!!
+class Instr(val lNames: Set<String>, val rNames: Set<String>)
+
+fun Instr.toString(cfg: CFG<Instr>): String {
+  val label = cfg.labeling[this]!!
+  if (cfg.reverse[this] == null)
+    println()
+  return when {
+    cfg.transitions[this]!!.isEmpty() -> "$label: <enter>"
+    (cfg.reverse[this] ?: emptyList()).isEmpty() -> "$label: <exit>"
+    this.lNames.isNotEmpty() -> "$label: " + this.lNames + " = " + this.rNames
+    else -> "$label: " + this.rNames
+  }
 }
-class BranchI(cfg: CFG<Instr>, val rNames: Set<String>): Instr(cfg)
-class AssgnI(cfg: CFG<Instr>, val lNames: Set<String>, val rNames: Set<String>): Instr(cfg)
-class EnterI(cfg: CFG<Instr>): Instr(cfg)
-class ExitI(cfg: CFG<Instr>): Instr(cfg)
 
 fun <T> printCFG(cfg: CFG<Instr>, anno: Map<Point,T>) {
   var nextLabels: List<String>? = null
@@ -18,12 +24,7 @@ fun <T> printCFG(cfg: CFG<Instr>, anno: Map<Point,T>) {
       println("  branches: " + nextLabels)
     println("---")
     println("in: " + anno[Pair(i, "in")])
-    when(i) {
-      is EnterI -> println("$label: <enter>")
-      is ExitI -> println("$label: <exit>")
-      is AssgnI -> println("$label: " + i.lNames + " = " + i.rNames)
-      is BranchI -> println("$label: " + i.rNames)
-    }
+    println(i.toString(cfg))
     println("out: " + anno[Pair(i, "out")])
     nextLabels = it.second.map { cfg.labeling[it]!! }
   }
@@ -31,27 +32,14 @@ fun <T> printCFG(cfg: CFG<Instr>, anno: Map<Point,T>) {
     println("  branches: " + nextLabels)
 }
 class SimpleProcessor: NodeProcessor<Instr> {
-  override fun enterInstr(cfg: CFG<Instr>): Instr {
-    return EnterI(cfg)
-  }
-  override fun assignInstr(cfg: CFG<Instr>, targets: Set<identifier>, refs: Set<String>): Instr {
-    return AssgnI(cfg, targets, refs)
-  }
-  override fun branchInstr(cfg: CFG<Instr>, refs: Set<String>): Instr {
-    return BranchI(cfg, refs)
-  }
-  override fun exitInstr(cfg: CFG<Instr>): Instr {
-    return ExitI(cfg)
+  override fun mkInstr(targets: Set<identifier>, refs: Set<String>): Instr {
+    return Instr(targets, refs)
   }
 }
 typealias Point = Pair<Instr,String>
 
 fun makeLiveVarProblem(cfg: CFG<Instr>): DFAProblem<Point,VarSet> {
-  fun genKillMaker(n: Instr) = when(n) {
-    is AssgnI -> Pair(n.rNames, n.lNames)
-    is BranchI -> Pair(n.rNames, emptySet())
-    else -> Pair(emptySet(), emptySet())
-  }
+  fun genKillMaker(n: Instr) = Pair(n.rNames, n.lNames)
   return makeSetDFAProblem(cfg, forward = false, must = false, init = ::emptySet, genKillMaker = ::genKillMaker)
 }
 
@@ -59,31 +47,27 @@ data class Val(val valName: String, val binder: String, val instLabel: String) {
   override fun toString() = valName
 }
 fun makeReachingDefsProblem(cfg: CFG<Instr>): DFAProblem<Point,Set<Val>> {
-  val valuesAcc = mutableMapOf<String,Int>()
+  val instrToDefsMap = calcValueDefs(cfg)
+  val defs = instrToDefsMap.values.flatten()
+  val nameToDefsMap = defs.groupBy { it.binder }.mapValues { it.value.toSet() }
+  fun genKillMaker(n: Instr): Pair<Set<Val>,Set<Val>> {
+    val gen = instrToDefsMap[n]!!
+    val kill = gen.flatMap { vl -> nameToDefsMap[vl.binder]!!.minus(vl) }.toSet()
+    return Pair(gen, kill)
+  }
+  return makeSetDFAProblem(cfg, true, false, ::emptySet, ::genKillMaker)
+}
+
+fun calcValueDefs(cfg: CFG<Instr>): Map<Instr, Set<Val>> {
+  val valuesAcc = mutableMapOf<String, Int>()
   fun newName(s: String): String {
     val i = valuesAcc.getOrDefault(s, -1) + 1
     valuesAcc[s] = i
     return s + "_" + i
   }
-  val instrToDefsMap = cfg.transitions.keys.toList().reversed().map { i ->
-    val vals = if (i is AssgnI) {
-      i.lNames.map { Val(newName(it), it, cfg.labeling[i]!!) }.toSet()
-    } else {
-      emptySet()
-    }
-    i to vals
+  return cfg.transitions.keys.toList().reversed().map { i ->
+    i to i.lNames.map { Val(newName(it), it, cfg.labeling[i]!!) }.toSet()
   }.toMap()
-  val defs = instrToDefsMap.values.flatten()
-  val nameToDefsMap = defs.groupBy { it.binder }.mapValues { it.value.toSet() }
-  fun genKillMaker(n: Instr): Pair<Set<Val>,Set<Val>> = when(n) {
-    is AssgnI -> {
-      val gen = instrToDefsMap[n]!!
-      val kill = gen.flatMap { vl -> nameToDefsMap[vl.binder]!!.minus(vl) }.toSet()
-      Pair(gen, kill)
-    }
-    else -> Pair(emptySet(), emptySet())
-  }
-  return makeSetDFAProblem(cfg, true, false, ::emptySet, ::genKillMaker)
 }
 
 fun <N> makeDominatorsProblem(cfg: CFG<N>): DFAProblem<Pair<N,String>,Set<N>> {
@@ -99,8 +83,7 @@ fun <N> makeDominatorsProblem(cfg: CFG<N>): DFAProblem<Pair<N,String>,Set<N>> {
   return makeSetDFAProblem(cfg, true, true, {nodes}, ::genKillMaker)
 }
 
-fun <N> calcIDom(cfg: CFG<N>, domsRes: Map<Pair<N,String>,Set<N>>): Map<N,N> {
-  val doms = domsRes.filterKeys { it.second == "out" }.mapKeys { it.key.first }
+fun <N> calcIDom(doms: Map<N,Set<N>>, cfg: CFG<N>): Map<N,N> {
   val sdoms = doms.mapValues { it.value.minus(it.key) }
   val res = mutableMapOf<N,N>()
   cfg.transitions.keys.forEach { n ->
@@ -109,20 +92,20 @@ fun <N> calcIDom(cfg: CFG<N>, domsRes: Map<Pair<N,String>,Set<N>>): Map<N,N> {
     if (r.size == 1)
       res[n] = r[0]
   }
-  return res
+  return res.toMap()
 }
 
 fun <N> calcDomFrontier(cfg: CFG<N>, idom: Map<N,N>): Map<N,Set<N>> {
   val nodes = cfg.transitions.keys
-  var res = nodes.map { b -> b to mutableSetOf<N>() }.toMap().toMutableMap()
+  val res = nodes.map { b -> b to mutableSetOf<N>() }.toMap().toMutableMap()
   nodes.forEach { b ->
     val preds = cfg.reverse[b] ?: emptyList()
     if (preds.size >= 2) {
       preds.forEach { p ->
         var runner = p
-        while (runner != idom[p]) {
+        while (runner != idom[b]) {
           res[runner]!!.add(b)
-          runner = idom[p]!!
+          runner = idom[runner]!!
         }
       }
     }
@@ -130,23 +113,314 @@ fun <N> calcDomFrontier(cfg: CFG<N>, idom: Map<N,N>): Map<N,Set<N>> {
   return res.mapValues { it.value.toSet() }.toMap()
 }
 
+fun insertPhiNodes(cfg: CFG<Instr>, phiNodes: Map<Instr,Set<String>>): Pair<CFG<Instr>, Map<Instr,Instr>> {
+  fun copy(i: Instr) = Instr(i.lNames, i.rNames)
+  val res = CFG<Instr>()
+  val dupInstrs = cfg.transitions.keys.map { it to copy(it) }.toMap()
+  val phiInstrs = phiNodes.mapValues { Instr(it.value, it.value) }
+  phiInstrs.forEach { (i, phiI) ->
+    res.addInstr(phiI, dupInstrs[i]!!)
+  }
+  cfg.transitions.forEach { (n, succ) ->
+    res.addInstr(dupInstrs[n]!!, succ.map { phiInstrs[it] ?: dupInstrs[it]!! })
+  }
+  res.complete()
+  return res to dupInstrs
+}
+
+fun <N> calcDom(cfg: CFG<N>)
+    = solve(makeDominatorsProblem(cfg))
+    .filterKeys { it.second == "out" }
+    .mapKeys { it.key.first }
+
+class DominanceAnalysis<N>(cfg: CFG<N>) {
+  val domsOf: Map<N,Set<N>> = calcDom(cfg)
+  val immDomOf: Map<N,N> = calcIDom(domsOf, cfg)
+  val domFrontier: Map<N, Set<N>> = calcDomFrontier(cfg, immDomOf)
+}
+
+class CFGAnalyses(cfg: CFG<Instr>) {
+  val dom: DominanceAnalysis<Instr> = DominanceAnalysis(cfg)
+  val liveVars: Map<Point,VarSet> = solve(makeLiveVarProblem(cfg))
+  val reachingDefs: Map<Point,Set<Val>> = solve(makeReachingDefsProblem(cfg))
+}
+
+class FuncAnalyses(f: FunctionDef) {
+  val cfg: CFG<Instr>
+  val cfgStmtMap: Map<Stmt,Pair<Instr,Instr>>
+  val edgeLabeling: Map<Pair<Instr,Instr>,EdgeSource>
+  val cfgAnalyses: CFGAnalyses
+  init {
+    val (_cfg, _stmtMap, _edgeLabeling) = CFGBuilder.makeCFG(f, SimpleProcessor())
+    cfg = _cfg
+    cfgStmtMap = _stmtMap
+    edgeLabeling = _edgeLabeling
+    cfgAnalyses = CFGAnalyses(cfg)
+  }
+}
+
+object CopiesComparator: Comparator<Pair<String,String>> {
+  override fun compare(o1: Pair<String, String>?, o2: Pair<String, String>?): Int {
+    val (a1, b1) = o1!!
+    val (a2, b2) = o2!!
+    return if (b1 == a2 && a1 == b2)
+      fail("curcular dependency $o1 $o2")
+    else if (a1 == b2)
+      1
+    else if (a2 == b1)
+      -1
+    else
+      0
+  }
+}
+fun makeCopyStmt(p: Pair<String,String>) = Assign(
+    targets = listOf(Name(id = p.second, ctx = ExprContext.Store)),
+    value = Name(id = p.first, ctx = ExprContext.Load))
+fun makeCopyStmts(s: List<Pair<String,String>>): List<Assign> {
+  val sortedWith = s.sortedWith(CopiesComparator)
+  return sortedWith.map(::makeCopyStmt)
+}
+
+fun insertCopies(s: Stmt, copies: Map<EdgeSource, List<Pair<String,String>>>): Stmt {
+  return when(s) {
+    is If -> s.copy(body = insertCopies(s.body, copies), orelse = insertCopies(s.orelse, copies))
+    is While -> s.copy(body = insertCopies(s.body, copies))
+    is For -> s.copy(body = insertCopies(s.body, copies))
+    else -> s
+  }
+}
+
+fun insertCopies(c: List<Stmt>, copies: Map<EdgeSource, List<Pair<String,String>>>): List<Stmt> {
+  val res = mutableListOf<Stmt>()
+  c.forEachIndexed { i, s ->
+    res.addAll(copies[BlockIndex(c, i)]?.let { makeCopyStmts(it) } ?: emptyList<Assign>())
+    res.add(insertCopies(s, copies))
+  }
+  res.addAll(copies[BlockIndex(c, c.size)]?.let { makeCopyStmts(it) } ?: emptyList<Assign>())
+  return res.toList()
+}
+
+fun renameFuncArg(a: Arg, renames: Map<String,String>) = a.copy(arg = renames[a.arg] ?: a.arg)
+fun renameFuncArguments(a: Arguments, renames: Map<String,String>) = a.copy(
+    posonlyargs = a.posonlyargs.map { renameFuncArg(it, renames) },
+    args = a.args.map { renameFuncArg(it, renames) },
+    vararg = a.vararg?.let { renameFuncArg(it, renames) },
+    kwonlyargs = a.kwonlyargs.map { renameFuncArg(it, renames) },
+    kw_defaults = a.kw_defaults.map { renameVars(it, renames) },
+    kwarg = a.kwarg?.let { renameFuncArg(it, renames) }
+)
+
 fun main() {
+  PyLib.init()
+  SSZLib.init()
+  BLS.init()
+
   val path = Paths.get("../eth2.0-specs/tests/fork_choice/defs_test.txt")
   val parsed = Files.readAllLines(path).map { ItemsParser2.parseToEnd(it) }
   val funcs = parsed.map { toStmt(it) as FunctionDef }
 
-  funcs.forEach {
-    val cfg = CFGBuilder.makeCFG(it, SimpleProcessor())
+  for (it in funcs) {
+    if (it.name != "test13")
+      continue
     println(it.name)
-    val lvp = makeLiveVarProblem(cfg)
-    val rdp = makeReachingDefsProblem(cfg)
-    val domsP = makeDominatorsProblem(cfg)
-    val domsRes = solve(domsP)
-    val idom = calcIDom(cfg, domsRes)
-    val domFr = calcDomFrontier(cfg, idom)
+    val newF = convertToAndOutOfSSA(it)
+    KotlinGen().genFunc(newF).forEach { println(it) }
+    /*println()
+    println("SSA")
+    val ssa = CFG<Instr>()
+    val instrMap = cfgWithPhis.transitions.keys.map { n ->
+      n to if (n in phiRenames) {
+        val inValues = reachDefsWithPhis[n to "in"]!!.groupBy { it.binder }
+        val phiVals = phiRenames[n]!!
+        Instr(phiVals.values.map { it.valName }.toSet(),
+            phiVals.keys.flatMap { inValues[it]!!.map { it.valName } }.toSet())
+      } else {
+        //val inRenames = inValues.mapValues { it.value[0] }
+        //val outRenames = reachDefsWithPhis[n to "out"]!!.groupBy { it.binder }.mapValues { it.value[0] }
+        val (inRenames, outRenames) = nonPhiRenames[n]!!
+        Instr(
+            n.lNames.map { outRenames[it]!!.valName }.toSet(),
+            n.rNames.map { inRenames[it]?.valName ?: it }.toSet())
+      }
+    }.toMap()
+    cfgWithPhis.transitions.forEach { (n, succ) ->
+      ssa.addInstr(instrMap[n]!!, succ.map { instrMap[it]!! })
+    }
+    ssa.complete()
 
-    printCFG(cfg, domFr.mapKeys { it.key to "out" })
+    printCFG(ssa, emptyMap<Point,Unit>())*/
+
     println()
   }
 
+}
+
+fun convertToAndOutOfSSA(it: FunctionDef): FunctionDef {
+  val analyses = FuncAnalyses(it)
+
+  val ssaInfo = calcSSARenames(it, analyses)
+  val enterRenames = ssaInfo.enter
+  val before = ssaInfo.before
+  val after = ssaInfo.after
+  val copies = ssaInfo.copies
+
+  val renamer = StmtRenamer(before, after)
+  val res = renamer.renameStmts(it.body)
+
+  fun remapEdgeSource(e: EdgeSource): EdgeSource = when (e) {
+    is BlockIndex -> e.copy(block = renamer.blockRemap[e.block]!!)
+    //is LoopTest -> e.copy(loop = renamer.stmtRemap[e.loop]!!)
+    is Undef -> e
+  }
+
+  val copies2 = copies.mapKeys { remapEdgeSource(it.key) }
+  val res2 = insertCopies(res, copies2)
+
+  val newF = it.copy(args = renameFuncArguments(it.args, enterRenames), body = res2)
+  return newF
+}
+
+typealias RENAMES = Map<String,String>
+typealias COPY = Pair<String,String>
+class SSAInfo(
+    val enter: RENAMES,
+    val before: StmtAnnoMap<RENAMES>,
+    val after: StmtAnnoMap<RENAMES>,
+    val copies: Map<EdgeSource, List<COPY>>
+)
+
+fun calcSSARenames(f: FunctionDef, analyses: FuncAnalyses): SSAInfo {
+  val cfg = analyses.cfg
+  val phiNodes = calcPhiNodes2(analyses)
+  val (cfgWithPhis, oldToNewInstrMap) = insertPhiNodes(cfg, phiNodes)
+  val phiNodes2 = cfgWithPhis.transitions.keys.minus(oldToNewInstrMap.values)
+
+  val phiAnalyses = CFGAnalyses(cfgWithPhis)
+  val reachDefsWithPhis = phiAnalyses.reachingDefs
+
+  /*val newRDomFr = reverse(phiAnalyses.dom.domFrontier)
+  val liveVarsWithPhis = phiAnalyses.liveVars
+  val _phiRenames = newRDomFr.keys.map { n ->
+    val preds = cfgWithPhis.reverse[n]!!
+    val rdefs = reachDefsWithPhis[n to "in"]!!
+    val live = liveVarsWithPhis[n to "in"]!!
+    val reaching = rdefs.map { it.binder }.toSet()
+    val shouldBeAvailableVars = live.intersect(reaching)
+    val nameToValsMap = shouldBeAvailableVars.map { it to mutableSetOf<Val>() }.toMap().toMutableMap()
+    preds.forEach { i ->
+      val vals = reachDefsWithPhis[i to "out"]!!.groupBy { it.binder }
+      shouldBeAvailableVars.forEach { v ->
+        val vs = vals[v] ?: fail("$v has no reaching definition at $i")
+        nameToValsMap[v]!!.addAll(vs)
+      }
+    }
+    val phiVars = n.lNames
+    n to reachDefsWithPhis[n to "out"]!!.groupBy { it.binder }.filterKeys { it in phiVars }.mapValues { it.value[0] }
+  }.toMap()*/
+
+  fun valsMap(vals: Collection<Val>, phiVars: Set<String>)
+      = vals.filter { it.binder in phiVars }
+      .groupBy { it.binder }
+      .mapValues { if (it.value.size != 1) fail() else it.value[0].valName }
+
+  val copies = phiNodes.flatMap { (n, vs) ->
+    val preds = cfg.reverse[n]!!
+    preds.flatMap { i ->
+      val edgeSource = analyses.edgeLabeling[i to n]!!
+      val pre = valsMap(reachDefsWithPhis[oldToNewInstrMap[i]!! to "out"]!!, vs)
+      val phi = valsMap(reachDefsWithPhis[oldToNewInstrMap[n]!! to "in"]!!, vs)
+
+      vs.map { edgeSource to (pre[it]!! to phi[it]!!) }
+    }
+  }.groupBy { it.first }.mapValues { it.value.map { it.second } }
+
+  val nonPhiRenames: Map<Instr, Pair<Map<String, Val>, Map<String, Val>>> = cfgWithPhis.transitions.keys.flatMap { n ->
+    if (n !in phiNodes2) {
+      val inValues = reachDefsWithPhis[n to "in"]!!.groupBy { it.binder }
+      val inRenames = inValues.mapValues { it.value[0] }
+      val outRenames = reachDefsWithPhis[n to "out"]!!.groupBy { it.binder }.mapValues { it.value[0] }
+      listOf(n to Pair(inRenames, outRenames))
+    } else {
+      emptyList()
+    }
+  }.toMap()
+
+  val before = StmtAnnoMap<Map<String,String>>()
+  val after = StmtAnnoMap<Map<String,String>>()
+  analyses.cfgStmtMap.forEach { (s, p) ->
+    val firstI = oldToNewInstrMap[p.first]!!
+    val lastI = oldToNewInstrMap[p.second]!!
+    before[s] = nonPhiRenames[firstI]!!.first.mapValues { it.value.valName }
+    if (s is Assign || s is AnnAssign || s is AugAssign) {
+      after[s] = nonPhiRenames[firstI]!!.second.mapValues { it.value.valName }
+    } else {
+      after[s] = nonPhiRenames[lastI]!!.second.mapValues { it.value.valName }
+    }
+  }
+
+  val funcInitI = cfgWithPhis.transitions[cfgWithPhis.first]!![0]
+  val initialRenames = reachDefsWithPhis[funcInitI to "out"]!!
+      .groupBy { it.binder }.mapValues { it.value[0].valName }
+  return SSAInfo(initialRenames, before, after, copies)
+}
+
+fun calcPhiNodes2(analyses: FuncAnalyses): Map<Instr, Set<String>> {
+  val cfg = analyses.cfg
+  val domFr = analyses.cfgAnalyses.dom.domFrontier
+  val liveVars = analyses.cfgAnalyses.liveVars
+  val instrToDefsMap = calcValueDefs(cfg)
+  val instrToNamesMap = instrToDefsMap.mapValues { it.value.map { it.binder } }
+  val nameToStmtsMap = reverse(instrToNamesMap)
+
+  val res = nameToStmtsMap.mapValues { (name, instrs) ->
+    val queue = instrs.toMutableList()
+    val res = mutableSetOf<Instr>()
+    var i = 0
+    while (i < queue.size) {
+      val instr = queue[i]
+      i += 1
+      val nodes = domFr.getOrDefault(instr, emptySet())
+      if (nodes.isNotEmpty()) {
+        for(n in nodes) {
+          if (name in liveVars[n to "in"]!!)
+            res.add(n)
+        }
+        queue.addAll(nodes.minus(queue.subList(0,i)))
+      }
+    }
+    res.toSet()
+  }
+  return reverse(res).mapValues { it.value.toSet() }
+}
+
+fun calcPhiNodes(analyses: FuncAnalyses): Map<Instr, Set<String>> {
+  val cfg = analyses.cfg
+  val liveVars = analyses.cfgAnalyses.liveVars
+  val reachingDefs = analyses.cfgAnalyses.reachingDefs
+  val doms = analyses.cfgAnalyses.dom.domsOf
+  val rdomFr = reverse(analyses.cfgAnalyses.dom.domFrontier)
+
+  val phiNodes = rdomFr.keys.map { n ->
+    val preds = cfg.reverse[n]!!
+    if (preds.any { n in doms[it]!! }) {
+      //println("cycle")
+    } else {
+      //println("non cycle")
+    }
+
+    val rdefs = reachingDefs[n to "in"]!!
+    val live = liveVars[n to "in"]!!
+    val reaching = rdefs.map { it.binder }.toSet()
+    val shouldBeAvailableVars = live.intersect(reaching)
+    val nameToValsMap = shouldBeAvailableVars.map { it to mutableSetOf<Val>() }.toMap().toMutableMap()
+    preds.forEach { i ->
+      val vals = reachingDefs[i to "out"]!!.groupBy { it.binder }
+      shouldBeAvailableVars.forEach { v ->
+        val vs = vals[v] ?: fail("$v has no reaching definition at $i")
+        nameToValsMap[v]!!.addAll(vs)
+      }
+    }
+    n to nameToValsMap.filter { it.value.size > 1 }.keys
+  }.toMap()
+  return phiNodes
 }
